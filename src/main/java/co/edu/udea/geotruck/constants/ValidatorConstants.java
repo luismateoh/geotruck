10d2242756faa2b5113d270f9c4e6626ff12fdf7
DESKTOP-87I8N0G\Paula Guarin <paula.guaring@udea.edu.co>
2021-10-08 20:29:13 -0500

    "validaciones"

--- src/main/java/co/edu/udea/geotruck/constants/ValidatorConstants.java
+++ src/main/java/co/edu/udea/geotruck/constants/ValidatorConstants.java
@@ -13,6 +13,12 @@ public class ValidatorConstants {
     public static final int MIN_SIZE_PASSWORD = 8;
     public static final int MAX_SIZE_PASSWORD = 15;
 
+    public static final int MIN_SIZE_ID = 5;
+    public static final int MAX_SIZE_ID = 12;
+
+    public static final int MIN_SIZE_LASTNAME = 1;
+    public static final int MAX_SIZE_LASTNAME = 25;
+
     //Placas colombia
     public static final String REGEX_PLACA =
             "^([A-Z]{3}\\d{3}|[A-Z]{2}\\d{4}|[A-Z]{2}\\d{3}[A-Z]|[A-Z]\\d{4,5})$";
@@ -32,6 +38,8 @@ public class ValidatorConstants {
     public static final String BAD_SIZE_EMAIL_MESSSAGE = "email must be between 5 and 20 characters";
     public static final String BAD_SIZE_PASSWORD_MESSAGE = "password must be between 8 and 15 characters";
     public static final String BAD_SIZE_NAME_MESSSAGE = "name must be between 5 and 20 characters";
+    public static final String BAD_SIZE_ID_MESSSAGE = "ID must be between 5 and 12 characters";
+    public static final String BAD_SIZE_LASTNAME_MESSSAGE = "ID must be between 1 and 25 characters";
     public static final String BAD_FORMAT_PLACA = "plate bad format";
     public static final String BAD_FORMAT_MODELO = "model out of range";
     public static final String BAD_FORMAT_LAT_LON= "latitude and longitude bad format";
