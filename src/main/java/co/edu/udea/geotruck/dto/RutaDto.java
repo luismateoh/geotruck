6b7aa729f1d7cb4db6a766e940ccd2555d775f90
joanri20 <dairo.rivas@udea.edu.co>
2021-10-08 19:37:05 -0500

    Creacion DTO

--- src/main/java/co/edu/udea/geotruck/dto/RutaDto.java
+++ src/main/java/co/edu/udea/geotruck/dto/RutaDto.java
@@ -12,23 +12,11 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 @NoArgsConstructor
-public class UserDto {
+public class RutaDto {
 
+    private String ciudadDestino;
+    private String ciudadOrigen;
     private Long id;
-
-    @NotNull
-    @Size(min = ValidatorConstants.MIN_SIZE_EMAIL, max = ValidatorConstants.MAX_SIZE_EMAIL, message = ValidatorConstants.BAD_SIZE_EMAIL_MESSSAGE)
-    @Pattern(regexp = ValidatorConstants.REGEX_EMAIL, message = ValidatorConstants.BAD_FORMAT_EMAIL_MESSSAGE)
-    private String email;
-
-    @NotNull
-    @Size(min = ValidatorConstants.MIN_SIZE_PASSWORD, max = ValidatorConstants.MAX_SIZE_PASSWORD, message = ValidatorConstants.BAD_SIZE_PASSWORD_MESSAGE)
-    private String password;
-    @Size(min = ValidatorConstants.MIN_SIZE_NAME, max = ValidatorConstants.MAX_SIZE_NAME, message = ValidatorConstants.BAD_SIZE_NAME_MESSSAGE)
-    private String nombre;
-
-    private String identificacion;
-
-    private String apellido;
+  
 
 }
